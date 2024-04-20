80313894fa9473b035f41f40681b8c3a4795f3c9
Andrii Gadar <gadar.andre@gmail.com>
2024-04-20 22:24:01 +0300

    Record & Omit<T, K>

--- src/index.ts
+++ src/index.ts
@@ -8,3 +8,45 @@ if (button && input) {
     concatenation(input.value, "hello!");
   });
 }
+
+type Callback = (...args: unknown[]) => unknown;
+
+function createLoggedFunction<T extends Callback>(func: T) {
+  let funcRef = func;
+
+  const loggedFunction = (...args: Parameters<T>) => {
+    console.log(`Function ${func.name} was called with arguments:`, args);
+    const result = funcRef(...args) as ReturnType<T>;
+    return result;
+  };
+
+  return loggedFunction;
+}
+
+export {};
+
+function greeting() {
+  return "Hello, world!";
+}
+
+type Greeting = ReturnType<typeof greeting>; // 'string'
+
+function multiply(a: number, b: number) {
+  return a * b;
+}
+
+type MultiplyResult = ReturnType<typeof multiply>; // 'number'
+
+export {};
+
+type InitialFormType = {
+  name: string;
+  email: string;
+  password: string;
+};
+
+export type Form = InitialFormType & {
+  errors: Partial<Record<keyof InitialFormType, [string]>>;
+};
+
+export {};
